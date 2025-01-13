40c3b2b335664d51122b0c4fb02b647669044e66
AGARCUC <agarcuc@alumno.upv.es@vrhpcadm1.dsic.upv.es>
2025-01-13 11:45:10 +0100

    Batch 96 ETTm1, experiments 1-8.

--- Deploys/batch_ETTm1_672.sh
+++ Deploys/batch_ETTm1_672.sh
@@ -2,19 +2,9 @@
 data="ETTm1"
 pred_horizon=672
 
-sbatch ./ETT/ex01.sh $data $pred_horizon
 sbatch ./ETT/ex02.sh $data $pred_horizon
 sbatch ./ETT/ex03.sh $data $pred_horizon
 sbatch ./ETT/ex04.sh $data $pred_horizon
 sbatch ./ETT/ex05.sh $data $pred_horizon
-sbatch ./ETT/ex06.sh $data $pred_horizon
-sbatch ./ETT/ex07.sh $data $pred_horizon
-sbatch ./ETT/ex08.sh $data $pred_horizon
 sbatch ./ETT/ex09.sh $data $pred_horizon
-sbatch ./ETT/ex10.sh $data $pred_horizon
-sbatch ./ETT/ex11.sh $data $pred_horizon
-sbatch ./ETT/ex12.sh $data $pred_horizon
 sbatch ./ETT/ex13.sh $data $pred_horizon
-sbatch ./ETT/ex14.sh $data $pred_horizon
-sbatch ./ETT/ex15.sh $data $pred_horizon
-sbatch ./ETT/ex16.sh $data $pred_horizon
