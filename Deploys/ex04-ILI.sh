8db4989a865688c7769ea467fcbb8c2d740931aa
Arnau Garcia i Cucó <arnau.g.cuco@gmail.com>
2025-01-28 19:44:52 +0100

    ex04-ILI.sh updated to save results properly

--- Deploys/ex04-ILI.sh
+++ Deploys/ex04-ILI.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source  ~/miniconda3/bin/activate KARMA
-result="$HOME/04-KARMA/results/$1/$2/01-$1-$2.out"
+result="$HOME/04-KARMA/results/$1/$2/01-$3-$4.out"
 python3 ../EX-ILI.py $1 $2 $3 $4 > $result
