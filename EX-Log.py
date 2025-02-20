db888e73a01b7d4c9499f9950cfede18a1a3bfda
Arnau Garcia i Cucó <arnau.g.cuco@gmail.com>
2025-02-20 14:27:21 +0100

    Experiment Logifruit ready

---
+++ EX-Log.py
@@ -0,0 +1,30 @@
+from Experiment1 import Experiment
+import sys
+
+dataset = "Logifruit"
+pred = int(sys.argv[1])
+AR_model = sys.argv[2]
+MA_model = sys.argv[3]
+
+endog = ["production"]
+
+exog = ["demand", "deliveries", "national_holidays", "provintial_holidays"]
+
+fourier = ["national_holidays", "provintial_holidays", 'fourier_sin_order1', 'fourier_cos_order1']
+
+
+
+
+X = {var:(0, 14) for var in endog}
+X.update({var:(0, 7) for var in exog})
+X.update({var:(-pred, 0) for var in fourier})
+
+model = Experiment(name=f"{dataset}/{pred}/{AR_model}-{MA_model}", 
+                   endogenous = endog,
+                   AR_model=AR_model,
+                   MA_model=MA_model,
+                   data_path="../../00-Data/"+dataset+".csv", 
+                   pred_horizon=pred, 
+                   context=X,
+                   errors_context=pred)
+model.fit()
